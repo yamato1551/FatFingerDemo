51cf5593bb7d1759809b72cdf08d14a9ccb95edc
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2019-05-28 16:40:21 +0900

    1.0

---
+++ FatFingerTest/Assets/Script/UIColumn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIColumn : MonoBehaviour
+{
+    public GameObject Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = (GameObject)Instantiate(Button);
+        Button.gameObject.transform.SetParent(this. transform);
+    }
+}

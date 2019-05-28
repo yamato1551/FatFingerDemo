9f328b00732da864bee2e9d6b7a2e6e26b22cfff
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2019-05-28 18:01:49 +0900

    1.0
    とりあえず動く

--- Assets/Script/UIColumn.cs
+++ Assets/Script/UIColumn.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIColumn : MonoBehaviour
 {
-    public GameObject Button;
+    GameObject Button;
+    GameObject canvas;
+    RectTransform rt;
+    Vector2 pos;
+    Vector2 margin;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject prefab = (GameObject)Instantiate(Button);
-        Button.gameObject.transform.SetParent(this. transform);
+        margin.x = 25;
+        margin.y = -25;
+        rt = GetComponent<RectTransform>();
+        pos = rt.anchoredPosition;
+        pos.x = -Screen.width * 0.5f + rt.rect.width * 0.5f + margin.x;
+        pos.y = Screen.height * 0.5f + rt.rect.height * 0.5f + margin.y;
+        Button = Resources.Load<GameObject>("Prefab/Button");
+        canvas = GameObject.Find("Canvas");
+        for (int i = 0; i < 10; i++)
+        {
+            margin.x +=25;
+            Placement();
+        }
+    }
+    void Placement()
+    {
+        Button = Instantiate(Button, pos, Quaternion.identity) as GameObject;
+        Button.transform.parent = canvas.transform;
     }
 }

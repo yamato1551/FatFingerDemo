70be6ae4cfe679595fa6456f94809c82241d4a9f
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2019-06-11 17:48:29 +0900

    1.6
    圧力によるズームインズームアウト実装

---
+++ Assets/Script/UI3DtouchZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI3DtouchZoom : MonoBehaviour
+{
+    private GridLayoutGroup content;
+    private Transform wrapper;
+    private RectTransform contentRect;
+    private float scale = 1;
+    public float min, max;
+    [System.Serializable]
+    struct RangeClass
+    {
+        public float min, max;
+    }
+    [SerializeField]
+    private RangeClass RangeScale;
+    [SerializeField]
+    private RangeClass RangeLimitedScale;
+     float ConvergenceTime;
+    bool isPitch;
+    void Start()
+    {
+        contentRect = content.GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        ConvergenceTime = Input.touches[0].pressure;
+    }
+    void EditorControl()
+    {
+        if (isPitch)
+        {
+            if (Input.touchCount > 0)
+            {
+                if()
+            }
+        }
+    }
+}
